e01996b1e38c04b3ed4fffe66e0f89de8d8f4a47
Aurelien Carlier <aureliencarlier123@gmail.com>
2016-06-04 18:10:50 +0200

    final version 2 with french resume

--- compilation_master.bat
+++ compilation_master.bat
@@ -13,13 +13,25 @@ CD ..
 :: process de compil tenant compte des multiples bibliographies
 :: pdflatex usefull options :
 ::      -draftmode 
+:: FOR %%f IN (master.aux, chap-*\*.aux) DO (bibtex %%f)
+
 pdflatex -interaction=errorstopmode master.tex
-FOR %%f IN (bu*.aux, master.aux) DO (bibtex %%f)
+
+bibtex master.aux
+bibtex chap-introduction\intro.aux
+bibtex chap-backgroundAndProblem\backgroundAndProblem.aux
+bibtex chap-sdp\sdp.aux
+bibtex chap-sdpExp\sdpExp.aux
+bibtex chap-slpAndEnergy\slpAndEnergy.aux
+bibtex chap-slpAndEnergyExp\slpAndEnergyExp.aux
+bibtex chap-conclusion\conclusion.aux
+bibtex publications\publications.aux
+
 pdflatex -interaction=errorstopmode master.tex
 pdflatex -interaction=errorstopmode master.tex
 
 :: clean du répertoire et des sous-répertoires
-FOR /r %%f IN (*.aux *.log bu*.* *.bbl *.blg *.mtc* *.out *.maf *.dvi *.toc *.synctex.gz) DO (DEL "%%f")
+FOR /r %%f IN (*.aux *.log bu*.* *.bbl *.blg *.mtc* *.out *.maf *.dvi *.toc *.synctex.gz *.lot *.lof) DO (DEL "%%f")
 
 :: open the master file
 start master.pdf
